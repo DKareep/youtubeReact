422454aa4906efeda227fc7abbdd2bff5ea06666
Dijin <dijin@lookup.to>
2016-06-02 21:22:30 +0530

    Initial commit, added components, setup youtube-api key

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// npm install --save youtube-api-search
+
+const API_KEY = "AIzaSyAiz--wlZwLBIGMJhd2qQuSH5L4jvZeDcE";
+const App = () => {
+    "use strict";
+    return <div>Hello there !!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
