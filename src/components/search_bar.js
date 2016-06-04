3b3623524ad5800f6538a67d5cb98f893992d4c3
Dijin <dijin@lookup.to>
2016-06-04 20:09:34 +0530

    event handler based on onChange function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,12 @@
-import React from 'react';
-class SearchBar extends React.Component {
+import React, {Component} from 'react';
+// const Component = React.Component;
+class SearchBar extends Component {
     render() {
         "use strict";
-        return <input/>;
-
+        return <input onChange={this.onInputChange} />;
+    }
+    onInputChange(event) {
+        console.log(event.target.value)
     }
 }
 export default SearchBar;
