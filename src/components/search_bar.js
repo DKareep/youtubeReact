e3c6711bcc81a301a9a7f1db8448213e44342678
Dijin <dijin@lookup.to>
2016-06-04 20:59:12 +0530

    YTSearch and setting state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 // const Component = React.Component;
 class SearchBar extends Component {
+    constructor (props) {
+        "use strict";
+        super(props);
+        this.state = {term: ''}
+    }
     render() {
         "use strict";
-        return <input onChange={(event) => {console.log(event)}} />;
+        return (
+            <div>
+                <input
+                    value = {this.state.value}
+                    onChange={(event) => this.setState({term: event.target.value})} />
+                value of input : {this.state.term}
+            </div>
+        );
     }
 }
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 //  npm install --save youtube-api-search
 import SearchBar from './components/search_bar'
 
+
 const API_KEY = "AIzaSyAiz--wlZwLBIGMJhd2qQuSH5L4jvZeDcE";
-const App = () => {
-    "use strict";
-    return (
-        <div>
-        <SearchBar />
-        </div>
-    );
-};
+
+class App extends Component {
+    constructor(props) {
+        "use strict";
+        super(props);
+        this.state = {
+            videos: []
+        };
+        YTSearch({
+                key: API_KEY,
+                term: 'Budha bar'
+            },
+            videos => {
+                "use strict";
+                this.setState({videos});
+            }
+        );
+    }
+
+    render() {
+        return (  <div>
+            <SearchBar />
+        </div>)
+    }
+
+;
+}
+;
 
 ReactDOM.render(<App />, document.getElementById('container'));
